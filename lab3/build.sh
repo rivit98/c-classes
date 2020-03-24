cc074cbb6693cfed3be82925815d8db295936e0b
rivit98 <albert98g@gmail.com>
2020-03-24 10:19:02 +0100

    build script changed

--- lab3/build.sh
+++ lab3/build.sh
@@ -1,1 +1,1 @@
-g++ -std=c++17 -Wextra -Weffc++ -Wall main.cpp  fraction.cpp  fractionTests.cpp vector.cpp vectorTests.cpp utils/argumentParsing.cc utils/programmersDetails.cc utils/memoryManagement.cpp -o kolokwium -lgtest -lpthread
+g++ -std=c++17 -Wextra -Weffc++ -Wall main.cpp  fraction.cpp  fractionTests.cpp vector.cpp vectorTests.cpp utils/memoryManagement.cpp -o main -lgtest -lpthread
